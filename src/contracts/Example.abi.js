024411a607258923af670e90af4196c9cffcbd12
Diana <diana.dev777@gmail.com>
2023-11-14 13:48:37 +0600

    feat: add string and data

--- src/contracts/Example.abi.js
+++ src/contracts/Example.abi.js
@@ -1,1 +1,44 @@
-export const ABI = [{"inputs":[{"internalType":"uint256","name":"_number","type":"uint256"}],"name":"addToData","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"getData","outputs":[{"internalType":"uint256[]","name":"","type":"uint256[]"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getNumber","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_number","type":"uint256"}],"name":"setNumber","outputs":[],"stateMutability":"nonpayable","type":"function"}]
+export const ABI = [
+  {
+    inputs: [],
+    name: 'getData',
+    outputs: [{ internalType: 'uint256[]', name: '', type: 'uint256[]' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'getNumber',
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'getString',
+    outputs: [{ internalType: 'string', name: '', type: 'string' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [{ internalType: 'uint256', name: '_data', type: 'uint256' }],
+    name: 'setData',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [{ internalType: 'uint256', name: '_number', type: 'uint256' }],
+    name: 'setNumber',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [{ internalType: 'string', name: '_str', type: 'string' }],
+    name: 'setString',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+];
--- src/contracts/example.sol
+++ src/contracts/example.sol
@@ -3,22 +3,31 @@
 pragma solidity 0.8.19;
 
 contract Example {
-  uint256 number;
-  uint256[] data;
+    uint256 number;
+    string str;
+    uint256[] data;
 
-  function setNumber(uint256 _number) public {
-    number = _number;
-  }
+    function setNumber(uint256 _number) public {
+        number = _number;
+    }
 
-  function getNumber() public view returns(uint256){
-    return number;
-  }
+    function setString(string calldata _str) public {
+        str = _str;
+    }
 
-  function addToData(uint256 _number) public {
-    data.push(_number);
-  }
+    function setData(uint256 _data) public {
+        data.push(_data);
+    }
 
-  function getData() public view returns(uint256[] memory){
-    return data;
-  }
+    function getNumber() public view returns (uint256) {
+        return number;
+    }
+
+    function getString() public view returns (string memory) {
+        return str;
+    }
+
+    function getData() public view returns (uint256[] memory) {
+        return data;
+    }
 }
